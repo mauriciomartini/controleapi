9ebdce5e9a094484829241e4c6df69583270424d
mauriciomartini <martinimauricio1@gmail.com>
2020-08-27 16:40:47 -0300

    Insert de Lançamento

--- src/main/java/br/com/controleapi/controller/LancamentoController.java
+++ src/main/java/br/com/controleapi/controller/LancamentoController.java
@@ -4,23 +4,22 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.controleapi.event.RecursoCriadoEvent;
 import br.com.controleapi.model.Lancamento;
+import br.com.controleapi.model.Pessoa;
 import br.com.controleapi.repository.LancamentoRepository;
-import br.com.controleapi.repository.filter.LancamentoFilter;
 import br.com.controleapi.service.LancamentoService;
 
 @RestController
@@ -44,6 +43,20 @@ public class LancamentoController {
         return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
     }
 
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarLancamento(@PathVariable Long codigo) {
+        lancamentoRepository.deleteBycodigo(codigo);
+        
+    }
+    
+    @PostMapping
+    public ResponseEntity<Lancamento> salvarLacamento(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+
+        Lancamento lancamentoSalva = lancamentoRepository.save(lancamento);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalva.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalva);
+    }
   
     
   
