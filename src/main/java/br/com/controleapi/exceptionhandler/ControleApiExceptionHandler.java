5b59b273122a89db2411f07b77e0282ca30bf9ca
mauriciomartini <martinimauricio1@gmail.com>
2020-08-27 16:56:00 -0300

    Ajuste mensagem de erro lancamentos

--- src/main/java/br/com/controleapi/exceptionhandler/ControleApiExceptionHandler.java
+++ src/main/java/br/com/controleapi/exceptionhandler/ControleApiExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -67,8 +68,8 @@ public class ControleApiExceptionHandler extends ResponseEntityExceptionHandler 
     @ExceptionHandler({ DataIntegrityViolationException.class })
     public ResponseEntity<Object> handleEmptyDataIntegrityViolationException (DataIntegrityViolationException ex, WebRequest request){
         String mensagemUsuario = messageSource.getMessage("recurso.operacao-na-permitida", null, LocaleContextHolder.getLocale());
-        String mensagemDesenvolvedor = ex.toString();
-        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+        String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
+         List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
         return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
     
--- src/main/java/br/com/controleapi/repository/PessoaRepository.java
+++ src/main/java/br/com/controleapi/repository/PessoaRepository.java
@@ -2,13 +2,16 @@ package br.com.controleapi.repository;
 
 
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.controleapi.model.Pessoa;
 
-
+@Transactional
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
     public Pessoa findBycodigo(Long codigo);
-    public Pessoa deleteBycodigo(Long codigo);
+    
+   
 
 }
